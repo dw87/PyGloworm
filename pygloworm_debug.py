429fc9c96076112f49d833ca183fc9f2f6941e32
DW <darren.wright87@googlemail.com>
2014-01-26 12:35:44 +0000

    Updated master Pygloworm code to output only barebones HTML structure to prevent browser from serving the page as a downloadable file. All other code to output debug information to HTML page removed.
    Moved debug version to pygloworm_debug.py.  Use this to test if script is being run successfully when called remotely.

--- pygloworm_debug.py
+++ pygloworm_debug.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 #
-print "Content-Type: text/html\n"
+print "Content-Type: text/html\n\n"
+print '<!DOCTYPE html>'
 
 from pyglow import PyGlow
 from time import sleep
@@ -16,12 +17,21 @@ randomLightsInt = random.randint(0,262143)
 
 randomLightBits = "{0:b}".format(randomLightsInt)
 
+#print randomLightBits
+
 i = 1
 leds = []
 for c in randomLightBits:
+	print c
 	if (int(c)*i>0):
 		leds.append(i)
 	i=i+1
 
 pyglow.set_leds(leds, 50)
 pyglow.update_leds()
+#print i
+
+print '<html>'
+print '<head><meta content="text/html; charset=UTF-8" /></head>'
+print '<body>Done</body>'
+print '</html>'
